6f00a271f68fbbfecf36ed8e2556dfd8fbd37c42
Egor88888888 <dyakovegor@gmail.com>
2025-05-12 17:48:53 +0300

    Fix Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,1 +1,13 @@
-{ "typescript": { "ignoreBuildErrors": true } }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    // cat next.config.js WARN cat next.config.js
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // cat next.config.js WARN cat next.config.js
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
